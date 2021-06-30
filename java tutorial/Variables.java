d3c900f22009d4b8302f81cb527deaf7cf0ddd64
AlgoTamizha <70270713+AlgoTamizha@users.noreply.github.com>
2021-06-30 20:37:12 +0530

    Create Variables.java

---
+++ java tutorial/Variables.java
@@ -0,0 +1,35 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int a = 5; // 4 bytes
+        a = 4;
+
+        String nam_e = "algo tamizha";// 12 * 2 = 24 bytes
+
+        char c = 'A'; // 2 bytes
+
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(c);
+
+        //primitive datatypes ()
+        byte b = 2;
+        int i = 10;
+        char s = 'w';
+
+        boolean available = false;
+
+        double d = 5.04;
+        System.out.println(d);
+
+        //constants
+        final int abc = 5;
+       // abc = 4;
+
+
+
+        
+
+    }
+}
